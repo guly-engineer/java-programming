49f2491ca911c023ba27b09a42443ce033f0cd7d
gbahar <gulbahar.tokus@gmail.com>
2021-11-29 23:59:26 +0000

    Change Class path to exercises folder

--- exercises/chapter8/PasswordValidatorGuly.java
+++ exercises/chapter8/PasswordValidatorGuly.java
@@ -1,4 +1,4 @@
-package chapter8;
+package exercises.chapter8;
 
 import java.util.ArrayList;
 import java.util.List;
--- exercises/chapter8/UserCredential.java
+++ exercises/chapter8/UserCredential.java
@@ -1,4 +1,4 @@
-package chapter8;
+package exercises.chapter8;
 
 public class UserCredential {
     String username;
